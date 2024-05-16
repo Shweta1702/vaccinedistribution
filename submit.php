161c10dc2a6e89cdcc137960b2252ec8c02d49e3
Shweta Mahajan <98207035+Shweta1702@users.noreply.github.com>
2024-05-16 09:06:07 +0300

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,9 @@
+<?php
+echo $_POST["VaccineName"]."</br>"; 
+echo $_POST["VaccineID"]."</br>"; 
+ echo $_POST["Supplier"]."</br>"; 
+echo $_POST["SupplierID"]."</br>"; 
+ echo $_POST["QuantityAvailable"]."</br>"; 
+ echo $_POST["QuantityOrdered"]."</br>"; 
+
+?>
